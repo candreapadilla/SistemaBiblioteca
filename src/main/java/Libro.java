b9654fdb4c9d9c578092ec0067b4589978fb9504
CamilaPadillaMontero <candreapadilla@unicesar.edu.co>
2026-02-26 15:50:30 -0500

    feat: agregar costructor y encapsulamiento a clase libro

--- src/main/java/Libro.java
+++ src/main/java/Libro.java
@@ -12,6 +12,65 @@ public class Libro {
     private String titulo;
     private String autor;
     private String genero;
-    private int aniopublicacion;
+    private int anioPublicacion;
     private boolean disponible;
+    
+    public Libro(String isbn, String titulo, String autor, String genero, int anioPublicacion, boolean disponible) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.anioPublicacion = anioPublicacion;
+        this.disponible = disponible;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
+    
 }
