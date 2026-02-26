c97a193c283a24898c172357c0c670e7baaafa55
CamilaPadillaMontero <candreapadilla@unicesar.edu.co>
2026-02-26 15:33:27 -0500

    feat: crear clase Libro con atributos

---
+++ src/main/java/Libro.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Camil
+ */
+public class Libro {
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private String genero;
+    private int aniopublicacion;
+    private boolean disponible;
+}
