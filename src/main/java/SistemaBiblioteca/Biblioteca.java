4fb3b86e09539f4bb8b4cf36ce9f8db8b3514bb9
CamilaPadillaMontero <candreapadilla@unicesar.edu.co>
2026-02-26 17:24:12 -0500

    feat: implementar UPDATE-acualizar libro

--- src/main/java/SistemaBiblioteca/Biblioteca.java
+++ src/main/java/SistemaBiblioteca/Biblioteca.java
@@ -44,5 +44,86 @@ public class Biblioteca {
 
         return null;
     }
+    
+    public void actualizarLibro(String isbn){
+
+    Libro libro = buscarLibro(isbn);
+
+    if(libro == null){
+        System.out.println("Libro no encontrado");
+        return;
+    }
+
+    Scanner sc = new Scanner(System.in);
+    int opcion;
+
+    do{
+        System.out.println("\n--- ACTUALIZAR LIBRO ---");
+        System.out.println("1. Titulo");
+        System.out.println("2. Autor");
+        System.out.println("3. Genero");
+        System.out.println("4. Año Publicacion");
+        System.out.println("5. Disponibilidad");
+        System.out.println("6. Actualizar TODO");
+        System.out.println("0. Salir");
+        System.out.print("Seleccione opcion: ");
+
+        opcion = sc.nextInt();
+        sc.nextLine(); // limpiar buffer
+
+        switch(opcion){
+
+            case 1:
+                System.out.print("Nuevo titulo: ");
+                libro.setTitulo(sc.nextLine());
+                break;
+
+            case 2:
+                System.out.print("Nuevo autor: ");
+                libro.setAutor(sc.nextLine());
+                break;
+
+            case 3:
+                System.out.print("Nuevo genero: ");
+                libro.setGenero(sc.nextLine());
+                break;
+
+            case 4:
+                System.out.print("Nuevo año: ");
+                libro.setAniopublicacion(sc.nextInt());
+                break;
+
+            case 5:
+                System.out.print("Disponible (true/false): ");
+                libro.setDisponible(sc.nextBoolean());
+                break;
+
+            case 6:
+                System.out.print("Nuevo titulo: ");
+                libro.setTitulo(sc.nextLine());
+
+                System.out.print("Nuevo autor: ");
+                libro.setAutor(sc.nextLine());
+
+                System.out.print("Nuevo genero: ");
+                libro.setGenero(sc.nextLine());
+
+                System.out.print("Nuevo año: ");
+                libro.setAniopublicacion(sc.nextInt());
+
+                System.out.print("Disponible (true/false): ");
+                libro.setDisponible(sc.nextBoolean());
+                break;
+
+            case 0:
+                System.out.println("Actualizacion finalizada");
+                break;
+
+            default:
+                System.out.println("Opcion invalida");
+        }
+
+    }while(opcion != 0);
+}
 }
  
